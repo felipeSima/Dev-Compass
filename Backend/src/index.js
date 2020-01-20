bd55423b222079a946ed7cbc1d1efea60549ced3
Felipe Lima <foux16@gmail.com>
2020-01-19 22:53:22 -0300

    //Added the Mobile application

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-const app = express();
 const cors = require('cors');
+const http = require('http');
+const {setupWebSocket} =  require('./websocket');
+
+const app = express();
+const server = http.Server(app);
 
 mongoose.connect('mongodb+srv://felipeSima:KaizokuniOreWaNaru26071611@cluster0-9y4nj.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -14,4 +18,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
